eb2a80e90382f838e668e68623aede7761b8d123
dsvlivon <52511637+dsvlivon@users.noreply.github.com>
2021-03-23 19:42:48 -0300

    ejercicio 11

---
+++ app/Ejercicio11.php
@@ -0,0 +1,16 @@
+<?php
+/******************************************************************************
+VIZGARRA.LIVON.DANIEL
+Aplicación No 11 (Potencias de números)
+Mostrar por pantalla las primeras 4 potencias de los números del uno 1 al 4 (hacer una función
+que las calcule invocando la función pow).
+
+*******************************************************************************/
+
+for($i=1;$i<5;$i++)
+{
+    for($j=$i;$j<5;$j++)
+    {
+        echo pow($i, $j);
+    }
+}
